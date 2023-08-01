c0c0b49dffbdc415f60f944972ec3ada1232d634
Sebastian Gnann <gnann@ad.umwelt.uni-potsdam.de>
2023-08-01 17:38:55 +0200

    maintenance

--- plot_data_bias.py
+++ plot_data_bias.py
@@ -13,7 +13,6 @@ if not os.path.isdir(results_path):
 # load and process data
 df = pd.read_csv("data/data_bias.csv", sep='\t')
 
-
 x = ["Kratzert", "Moeck", "Fan", "Global"]
 #x = ['Global', 'Streamflow (Kratzert et al., 2023)', 'Groundwater recharge (Moeck et al., 2020)', 'Water table depth (Fan et al., 2013)']
 y0 = 100*df.loc[0].values[1:]
@@ -45,16 +44,24 @@ df = pd.read_csv("data/data_bias_aridity.csv", sep='\t')
 
 x = ["Kratzert", "Moeck", "Fan", "Global"]
 #x = ['Global', 'Streamflow (Kratzert et al., 2023)', 'Groundwater recharge (Moeck et al., 2020)', 'Water table depth (Fan et al., 2013)']
+#y0 = 100*df.loc[0].values[1:]
+#y1 = 100*df.loc[1].values[1:]
+#y2 = 100*df.loc[2].values[1:]+1 # to compensate for rounding error
+#y3 = 100*df.loc[3].values[1:]+1
 y0 = 100*df.loc[0].values[1:]
 y1 = 100*df.loc[1].values[1:]
 y2 = 100*df.loc[2].values[1:]+1 # to compensate for rounding error
 y3 = 100*df.loc[3].values[1:]+1
 fig = plt.figure(figsize=(4, 2), constrained_layout=True)
 width = 0.5
-bar1 = plt.bar(x, y0, width=width, color='#01665e', label='Humid plains')
-bar2 = plt.bar(x, y1, bottom=y0, width=width, color='#80cdc1', label='Arid plains')
-bar3 = plt.bar(x, y2, bottom=y0+y1, width=width, color='#8c510a', label='Humid uplands')
-bar4 = plt.bar(x, y3, bottom=y0+y1+y2, width=width, color='#dfc27d', label='Arid uplands')
+#bar1 = plt.bar(x, y0, width=width, color='#01665e', label='Humid plains')
+#bar2 = plt.bar(x, y1, bottom=y0, width=width, color='#80cdc1', label='Arid plains')
+#bar3 = plt.bar(x, y2, bottom=y0+y1, width=width, color='#8c510a', label='Humid uplands')
+#bar4 = plt.bar(x, y3, bottom=y0+y1+y2, width=width, color='#dfc27d', label='Arid uplands')
+bar1 = plt.bar(x, y0, width=width, color='#2c7bb6', label='Humid plains')
+bar3 = plt.bar(x, y2, bottom=y0, width=width, color='#abd9e9', label='Humid uplands')
+bar2 = plt.bar(x, y1, bottom=y0+y2, width=width, color='#d7191c', label='Arid plains')
+bar4 = plt.bar(x, y3, bottom=y0+y1+y2, width=width, color='#fdae61', label='Arid uplands')
 #plt.xlabel("Landform")
 plt.ylabel('Percentage %')
 plt.ylim([0,100])
